ed0ec03df3674a46dc609390d0516351f46cfc33
Minehiko Nohara <minepicco@gmail.com>
2024-09-14 04:16:50 +0900

    Update toplevel.mk

--- OpenWrt/openwrt-18.06.2/include/toplevel.mk
+++ OpenWrt/openwrt-18.06.2/include/toplevel.mk
@@ -133,11 +133,11 @@ oldconfig: scripts/config/conf prepare-tmpinfo FORCE
 		$< --$(if $(confdefault),$(confdefault),old)config Config.in
 
 menuconfig: scripts/config/mconf prepare-tmpinfo FORCE
-#	if [ \! -e .config -a -e $(HOME)/.openwrt/defconfig ]; then \
-#		cp $(HOME)/.openwrt/defconfig .config; \
-#	fi
-#	[ -L .config ] && export KCONFIG_OVERWRITECONFIG=1; \
-#		$< Config.in
+	if [ \! -e .config -a -e $(HOME)/.openwrt/defconfig ]; then \
+		cp $(HOME)/.openwrt/defconfig .config; \
+	fi
+	[ -L .config ] && export KCONFIG_OVERWRITECONFIG=1; \
+		$< Config.in
 
 xconfig: scripts/config/qconf prepare-tmpinfo FORCE
 	if [ \! -e .config -a -e $(HOME)/.openwrt/defconfig ]; then \
